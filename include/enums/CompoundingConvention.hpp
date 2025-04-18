cb918ea43d86867ea72660e9d0e2857afba7c355
ItoWindsor <itowindsor@gmail.com>
2025-04-18 20:46:55 +0200

    ADD [FEATURE|Bonds] : Started the distinction to compute bonds using different compounding and day count convention

---
+++ include/enums/CompoundingConvention.hpp
@@ -0,0 +1,9 @@
+#ifndef COMPOUNGINDCONVENTION_HPP
+#define COMPOUNGINDCONVENTION_HPP
+
+enum class CompoundingConvention {
+  continuous,
+  exponential
+};
+
+#endif
---
+++ include/enums/DayCountConvention.hpp
@@ -0,0 +1,8 @@
+#ifndef DAYCOUNTCONVENTION_HPP
+#define DAYCOUNTCONVENTION_HPP
+
+enum class DayCountConvention {
+    Actual360,
+};
+
+#endif
