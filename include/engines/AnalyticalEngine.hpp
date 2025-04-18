5a638344be302c925ddfbaa4c881839ed6a9ec7b
ItoWindsor <itowindsor@gmail.com>
2025-04-18 23:00:05 +0200

    UPDATE [FEATURE|Bonds] : Fix of a bad import and added a default constructor

--- include/engines/AnalyticalEngine.hpp
+++ include/engines/AnalyticalEngine.hpp
@@ -4,11 +4,12 @@
 #include <memory>
 #include "core/MarketData.hpp"
 #include "core/Instrument.hpp"
-#include "instruments/bond.hpp"
+#include "instruments/Bond.hpp"
 
 class AnalyticalEngine {
   
   public:
+    AnalyticalEngine() = default;
     AnalyticalEngine(std::shared_ptr<MarketData> market_data);
     double compute_price(const Bond& bond);
     double compute_sensitiviies(const Instrument& instrument);
