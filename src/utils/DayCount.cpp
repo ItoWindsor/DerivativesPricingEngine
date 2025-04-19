f7783ff27b31fa61c8319f7b89fbcf9097ba6f16
ItoWindsor <itowindsor@gmail.com>
2025-04-19 19:41:35 +0200

    UPDATE [FEATURE|Bonds] : Added the computation for a day count curve + some tests

--- src/utils/DayCount.cpp
+++ src/utils/DayCount.cpp
@@ -1,5 +1,9 @@
 #include <chrono>
+#include <vector>
+#include <tuple>
 #include "utils/DayCount.hpp"
+#include "core/InterestRateCurve.hpp"
+#include "enums/DayCountConvention.hpp"
 
 double year_fraction(
   std::chrono::sys_days start,
@@ -7,12 +11,28 @@ double year_fraction(
   DayCountConvention convention) {
 
   std::chrono::days days_duration = std::chrono::duration_cast<std::chrono::days>(end - start);
-    double days = days_duration.count();
+  double days = days_duration.count();
 
-    switch (convention) {
-        case DayCountConvention::Actual360:
-            return static_cast<double>(days) / 360.0;
-        case DayCountConvention::Actual365:
-            return static_cast<double>(days) / 365.0;
-    }
+  switch (convention) {
+      case DayCountConvention::Actual360:
+          return static_cast<double>(days) / 360.0;
+      case DayCountConvention::Actual365:
+          return static_cast<double>(days) / 365.0;
+  }
+}
+
+std::vector<std::tuple<double,double>>  year_fraction(
+  std::chrono::sys_days valuation_date,
+  const InterestRateCurve& curve,
+  DayCountConvention convention
+  ) {
+  
+  auto original_curve = curve.get_curve();
+  std::vector<std::tuple<double,double>> vec_data_curve(original_curve.size(), {0.0,0.0});
+  for (const auto& [date, rate] : original_curve) {
+          double yf = year_fraction(valuation_date, date, convention);
+          vec_data_curve.emplace_back(yf, rate);
+      }
+  
+  return vec_data_curve;
 }
--- tests/test_day_count_convention.cpp
+++ tests/test_day_count_convention.cpp
@@ -2,8 +2,19 @@
 
 #include <chrono>
 #include "enums/DayCountConvention.hpp"
+#include "utils/DayCount.hpp"
 
-TEST(DayCountConvention, time_to_maturity){
-  EXPECT_EQ(1+1,2);
+TEST(DayCountConventionTest, TimeToMaturity) {
+  std::chrono::year_month_day start_ymd{std::chrono::year{2023}, std::chrono::month{4}, std::chrono::day{1}};
+  std::chrono::year_month_day end_ymd{std::chrono::year{2023}, std::chrono::month{10}, std::chrono::day{1}};
+
+  std::chrono::sys_days start_date{start_ymd};
+  std::chrono::sys_days end_date{end_ymd};
+
+  double actual360 = year_fraction(start_date, end_date, DayCountConvention::Actual360);
+  double actual365 = year_fraction(start_date, end_date, DayCountConvention::Actual365);
+
+  EXPECT_DOUBLE_EQ(actual360, 183.0 / 360.0);
+  EXPECT_DOUBLE_EQ(actual365, 183.0 / 365.0);
 }
 
