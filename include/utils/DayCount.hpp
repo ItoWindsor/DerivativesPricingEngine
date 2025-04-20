3da09966ad664f0ad1c0f7cd4ecb7aca057ec7c8
ItoWindsor <itowindsor@gmail.com>
2025-04-21 00:44:25 +0200

    UPDATE [FEATURE|Bonds] : Added day count functions to compute the time difference

--- include/utils/DayCount.hpp
+++ include/utils/DayCount.hpp
@@ -2,11 +2,32 @@
 #define DAYCOUNT_HPP
 
 #include <chrono>
+#include <tuple>
+#include <vector>
 #include "enums/DayCountConvention.hpp"
+#include "enums/CompoundingConvention.hpp"
 
-double year_fraction(
-    std::chrono::sys_days start,
-    std::chrono::sys_days end,
-    DayCountConvention convention);
+double compute_year_fraction(
+  std::chrono::sys_days start,
+  std::chrono::sys_days end,
+  DayCountConvention convention);
+
+
+std::vector<std::tuple<double,double>> compute_year_fraction(
+  std::chrono::sys_days valuation_date,
+  const std::vector<std::tuple<std::chrono::sys_days,double>>& curve_data,
+  DayCountConvention convention);
+
+std::vector<double> compute_year_fraction(
+  std::chrono::sys_days valuation_date,
+  const std::vector<std::chrono::sys_days>& vec_date, 
+  DayCountConvention convention);
+
 
 #endif
+
+double compute_discount_factor( 
+  double yield,
+  double time_to_maturity,
+  CompoundingConvention compounding_convention
+);
--- src/utils/DayCount.cpp
+++ src/utils/DayCount.cpp
@@ -2,10 +2,9 @@
 #include <vector>
 #include <tuple>
 #include "utils/DayCount.hpp"
-#include "core/InterestRateCurve.hpp"
 #include "enums/DayCountConvention.hpp"
 
-double year_fraction(
+double compute_year_fraction(
   std::chrono::sys_days start,
   std::chrono::sys_days end,
   DayCountConvention convention) {
@@ -21,18 +20,52 @@ double year_fraction(
   }
 }
 
-std::vector<std::tuple<double,double>>  year_fraction(
+std::vector<std::tuple<double,double>> compute_year_fraction(
   std::chrono::sys_days valuation_date,
-  const InterestRateCurve& curve,
+  const std::vector<std::tuple<std::chrono::sys_days,double>>& curve_data,
   DayCountConvention convention
   ) {
   
-  auto original_curve = curve.get_curve();
-  std::vector<std::tuple<double,double>> vec_data_curve(original_curve.size(), {0.0,0.0});
-  for (const auto& [date, rate] : original_curve) {
-          double yf = year_fraction(valuation_date, date, convention);
+  std::vector<std::tuple<double,double>> vec_data_curve(curve_data.size(), {0.0,0.0});
+  for (const auto& [date, rate] : curve_data) {
+          double yf = compute_year_fraction(valuation_date, date, convention);
           vec_data_curve.emplace_back(yf, rate);
-      }
-  
+      } 
   return vec_data_curve;
+}
+
+
+std::vector<double> compute_year_fraction(
+  std::chrono::sys_days valuation_date,
+  const std::vector<std::chrono::sys_days>& vec_date,
+  DayCountConvention convention
+  ) {
+  
+  std::vector<double> vec_time(vec_date.size(), {0.0});
+  for (const auto& date : vec_date) {
+          double yf = compute_year_fraction(valuation_date, date, convention);
+          vec_time.emplace_back(yf);
+      } 
+  return vec_time;
+}
+
+double compute_discount_factor(
+  double yield,
+  double time_to_maturity,
+  CompoundingConvention compounding_convention){
+  
+  double df = 0.0;
+  switch (compounding_convention) {
+    case CompoundingConvention::continuous:
+      df = std::exp(-yield*time_to_maturity);
+    case CompoundingConvention::actuarial_anually:
+      df = std::pow(1+yield,-time_to_maturity);
+    case CompoundingConvention::actuarial_semianually:
+      df = std::pow(1+yield/2.0,-2.0*time_to_maturity);
+    case CompoundingConvention::actuarial_quarterly:
+      df = std::pow(1+yield/4.0,-4.0*time_to_maturity);
+    case CompoundingConvention::actuarial_monthly:
+      df = std::pow(1+yield/12.0,-12.0*time_to_maturity);
+  }
+  return df;
 }
