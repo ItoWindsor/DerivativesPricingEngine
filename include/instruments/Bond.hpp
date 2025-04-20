96e25b2a9358154eeaa7a7f4933d983f4e584a4f
ItoWindsor <itowindsor@gmail.com>
2025-04-21 00:46:04 +0200

    UPDATE [FEATURE|Bonds] : Added some elements for Bond's constructor

--- include/instruments/Bond.hpp
+++ include/instruments/Bond.hpp
@@ -3,6 +3,8 @@
 
 #include <chrono>
 #include "core/Instrument.hpp"
+#include "enums/CompoundingConvention.hpp"
+#include "enums/DayCountConvention.hpp"
 
 class Bond : public Instrument{
   public:
@@ -10,15 +12,22 @@ class Bond : public Instrument{
       std::chrono::sys_days start_date,
       std::chrono::sys_days valuation_date,
       std::chrono::sys_days maturity_date,
+      DayCountConvention day_count_convention,
+      CompoundingConvention CompoundingConvention,
       double Nominal,
+      double coupon_rate,
       double spread,
       std::vector<std::chrono::sys_days> vec_date_cashflow
     );    
     double get_nominal() const;
     double get_spread() const;
-    std::vector<std::chrono::sys_days>& get_vec_date_cashflows();
+    double get_coupon_rate() const;
+    CompoundingConvention get_compounding_convention() const;
+    const std::vector<std::chrono::sys_days>& get_vec_date_cashflows() const;
   private:
+    CompoundingConvention compounding_convention;
     double nominal;
+    double coupon_rate;
     double spread;
     std::vector<std::chrono::sys_days> vec_date_cashflow; 
 };
