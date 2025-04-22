963842db7cc45e9b8440d287817848382f7222c4
ItoWindsor <itowindsor@gmail.com>
2025-04-22 11:53:00 +0200

    CHORE : testing << operator for the bond

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <chrono>
+#include "enums/CompoundingConvention.hpp"
+#include "instruments/Bond.hpp"
+
 
 int main(){
-  std::cout << "Hello World!" << std::endl;
+  std::chrono::sys_days start_date = std::chrono::year{2025} / std::chrono::month{1} / std::chrono::day{1}; 
+  std::chrono::sys_days valuation_date = start_date;
+  std::chrono::sys_days maturity_date = std::chrono::year{2025} / std::chrono::month{5} / std::chrono::day{12};
+  CompoundingFrequency compounding_frequency = CompoundingFrequency::monthly;
+  double nominal = 100;
+  double coupon_rate = 0.02;
+  double spread = 0.0;
+  
+  Bond bond(
+  start_date,
+  valuation_date,
+  maturity_date,
+  nominal,
+  coupon_rate,
+  spread,
+  DayCountConvention::Actual365,           
+  CompoundingMethod::actuarial,            
+  compounding_frequency,           
+  ShortSchedule::ShortEnd                 
+  );
+
+  std::cout << bond << std::endl;
 }
