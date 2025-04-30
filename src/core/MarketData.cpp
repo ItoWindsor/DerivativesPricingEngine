60b4e2ea45e224b3f19b3f5956a82810e50b977e
ItoWindsor <itowindsor@gmail.com>
2025-04-30 18:53:54 +0200

    UPDATE [FEATURE|BlackScholes] : Update of the market data -> can now hold stock prices

--- src/core/MarketData.cpp
+++ src/core/MarketData.cpp
@@ -8,3 +8,21 @@ std::shared_ptr<InterestRateCurve> MarketData::get_rate_curve() const{
 void MarketData::load_interest_rate_curve(const std::string& filepath, std::chrono::sys_days valuation_date) {
     interestRateCurve = std::make_shared<InterestRateCurve>(filepath, valuation_date);
 }
+
+void MarketData::load_stock_prices(const std::string& filepath){
+
+}
+
+void MarketData::load_stock_prices(const std::vector<std::tuple<std::string, double>>& stock_prices) {
+  for (auto& [name, price] : stock_prices) {
+    this->stockPrices[std::move(name)] = price;
+  }
+}
+
+std::unordered_map<std::string, double> MarketData::get_stock_prices() const{
+  return this->stockPrices;
+}
+  
+double MarketData::get_stock_price(const std::string& stock_name) const{
+  return this->stockPrices.at(stock_name);  
+}
