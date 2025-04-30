37ca8e1f892ee227056d855c8cea852e25942f53
ItoWindsor <itowindsor@gmail.com>
2025-04-30 19:04:34 +0200

    CHORE : to do

--- tests/test_black_scholes.cpp
+++ tests/test_black_scholes.cpp
@@ -2,6 +2,16 @@
 #include "models/BlackScholeModel.hpp"
 #include "engines/AnalyticalEngine.hpp"
 
-TEST(BlackScholesTest, CallAnalyticalPricing){
+TEST(BlackScholesTest, temp){
   EXPECT_EQ(1+1,2);
 }
+
+//TEST(BlackScholesTest, CallAnalyticalPricing){
+//  double spot = 100.0;
+//  double sigma = 0.2;
+//  double r = 0.04;
+//
+//  BlackScholesModel model = BlackScholesModel(spot,sigma,r);
+//  AnalyticalEngine engine;
+//
+//}
