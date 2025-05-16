062272a9e67adcc753863b861654a2cce848d90f
ItoWindsor <itowindsor@gmail.com>
2025-05-16 23:59:45 +0200

    UPDATE [FEATURE|AmericanOptions] : added LS to price an option

--- include/engines/MonteCarloEngine.hpp
+++ include/engines/MonteCarloEngine.hpp
@@ -8,21 +8,26 @@
 #include "core/UnderlyingModel.hpp"
 #include "instruments/equity/Option.hpp"
 #include "structs/OptionPriceResult.hpp"
+#include "structs/BasisFunctions.hpp"
 
 class MonteCarloEngine : public PricingEngine{
   public:
     MonteCarloEngine() = default;
-    MonteCarloEngine(int n_sim, int n_step);
+    MonteCarloEngine(int n_sim, int n_step = 1);
     MonteCarloEngine(std::shared_ptr<MarketData> market_data);
     MonteCarloEngine(std::shared_ptr<MarketData> market_data, std::shared_ptr<UnderlyingModel> underlying_model);
     OptionPriceResult compute_price(const Option& option) const;
     void set_n_simulation(int n_sim);
     void set_n_steps(int n_step);
+    void set_basis_functions(const BasisFunctions& basis);
 
     int get_n_sim() const;
+    int get_n_steps() const;
+    const BasisFunctions& get_basis_functions() const;
   private:
     int n_simulation;
     int n_steps;
+    BasisFunctions basis_functions; 
 };
 
 #endif
